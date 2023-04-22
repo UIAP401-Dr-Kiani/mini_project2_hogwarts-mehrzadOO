47617941e362b6152a1e1c8e69717e0f7eb6cf60
mehrzad ansaripoor <mehrzadansaripoor10@gmail.com>
2023-04-22 21:30:49 +0430

    added intil main page

--- MyProject02/Program.cs
+++ MyProject02/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace MyProject02
+﻿using System;
+
+namespace MyProject02
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static void MainPage()
         {
-            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Welcome to hogwarts school:)");
+            Console.ResetColor();
+            Console.WriteLine("1. Dambeldorn (d)");
+            Console.WriteLine("2. Teacher (t)");
+            Console.WriteLine("3. Student (s)");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Please go to your panel:)");
+            try
+            {
+                string MainInpute = Console.ReadLine();
+                if (MainInpute == "d")
+                {
+                    Console.Clear();
+                }
+                else if (MainInpute == "t") 
+                {
+                    Console.Clear();
+                }
+                else if (MainInpute == "s")
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You enter wrong key:( please enter true key:)");
+                    Console.ResetColor();
+                    MainPage();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.Clear();
+                Console.WriteLine("You enter wrong key:( please enter true key:)");
+                MainPage();
+
+            }
+        }
+        public static void Main(string[] args) {
+            MainPage();   
         }
     }
 }
