e25281cf9e8e3266f2a289ddf2ae0d40bbcb6e9f
mehrzad ansaripoor <mehrzadansaripoor10@gmail.com>
2023-04-22 22:00:45 +0430

    added until student main page.

--- MyProject02/Program.cs
+++ MyProject02/Program.cs
@@ -4,12 +4,53 @@ namespace MyProject02
 {
     internal class Program
     {
+        public static void StudentMainPage()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello student.please login:)");
+            Console.ResetColor();
+            Console.WriteLine("1. signin (i)");
+            Console.WriteLine("2. signup (u)");
+            try
+            {
+                string StudentMainInpute = Console.ReadLine();
+                if (StudentMainInpute == "i")
+                {
+                    Console.Clear();
+                }
+                else if (StudentMainInpute == "u")
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You enter wrong key:( please enter true key:)");
+                    Console.ResetColor();
+                    StudentMainPage();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You enter wrong key:( please enter true key:)");
+                Console.ResetColor();
+                StudentMainPage();
+                
+            }
+        }
+        public static void DambeldorPage()
+        {
+            
+        }
         public static void MainPage()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Welcome to hogwarts school:)");
             Console.ResetColor();
-            Console.WriteLine("1. Dambeldorn (d)");
+            Console.WriteLine("1. Dambeldor (d)");
             Console.WriteLine("2. Teacher (t)");
             Console.WriteLine("3. Student (s)");
             Console.ForegroundColor = ConsoleColor.Blue;
@@ -28,6 +69,7 @@ namespace MyProject02
                 else if (MainInpute == "s")
                 {
                     Console.Clear();
+                    StudentMainPage();
                 }
                 else
                 {
